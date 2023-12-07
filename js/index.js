86d7041894a3cec1ac7c159f88f7d9e585c44a52
elisa-daria <elisa.daria.tomasi@gmail.com>
2023-12-07 17:09:11 +0100

    let instead of const

--- js/index.js
+++ js/index.js
@@ -2,12 +2,10 @@ const button = document.querySelector("button");
 button.addEventListener("submit", function (e) {
   e.preventDefault();
 });
-const generateNumber = button.addEventListener("submit", function () {
-  return randomicNumber;
+const generateNumber = button.addEventListener("click", function () {
+  let randomicNumber = Math.ceil(Math.random() * 99);
+  console.log(randomicNumber);
 });
-
-const randomicNumber = Math.ceil(Math.random() * 99);
-console.log(randomicNumber);
 
 const createNumberCell = function () {
   const tombolaDiv = document.getElementById("tombola-container");
