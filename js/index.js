352320240acb368d1af32895685d4a771783f225
elisa-daria <elisa.daria.tomasi@gmail.com>
2023-12-09 00:32:01 +0100

    solved spanNumber

--- js/index.js
+++ js/index.js
@@ -2,26 +2,37 @@ const button = document.querySelector("button");
 button.addEventListener("submit", function (e) {
   e.preventDefault();
 });
-const generateNumber = button.addEventListener("click", function () {
-  let randomicNumber = Math.ceil(Math.random() * 99);
-  console.log(randomicNumber);
-  const spanNumber = document.getElementById("generatedNumber");
-  const numberGenerated = document.createElement("span");
-  numberGenerated.innerHTML = randomicNumber;
 
-  spanNumber.appendChild(numberGenerated);
-});
+let randomicNumber;
 
 const createNumberCell = function () {
   const tombolaDiv = document.getElementById("tombola-container");
-
-  for (let i = 1; i <= 99; i++) {
+  for (let i = 1; i <= 76; i++) {
     const tombolaCell = document.createElement("div");
+
     tombolaCell.classList.add("cells");
     const cellValue = document.createElement("h3");
     cellValue.innerText = i;
+
+    if (i === randomicNumber) {
+      tombolaCell.classList.add("selected");
+    }
     tombolaCell.appendChild(cellValue);
     tombolaDiv.appendChild(tombolaCell);
   }
 };
+
+const generateNumber = button.addEventListener("click", function () {
+  randomicNumber = Math.ceil(Math.random() * 76);
+  console.log(randomicNumber);
+
+  const spanNumber = document.getElementById("generatedNumber");
+  spanNumber.innerHTML = "";
+  const numberGenerated = document.createElement("span");
+
+  numberGenerated.innerHTML = randomicNumber;
+
+  spanNumber.appendChild(numberGenerated);
+  //createNumberCell();se la chiamo qui mi da lo stile ma genera nuove tabelle con il clik al bottone
+});
 createNumberCell();
